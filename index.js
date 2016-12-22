abc3833d5687ba9575120e8230616a0c90a3fdbe
Daniel Valencia <dvalencia@tacitknowledge.com>
2016-12-21 21:33:39 -0600

    Refactoring code a bit. Now we're generating TGF file

--- index.js
+++ index.js
@@ -1,114 +1,4 @@
-const client = require('./twitterClient')
-const CacheManager = require('./cache')
-const cache = new CacheManager('myproject')
-
-const getFollowersIdsForUser = (user) => {
-  const endpoint = 'followers/ids'
-
-  let params = {}
-  if (typeof user === 'number') {
-    params = {
-      user_id: user
-    }
-  } else {
-    params = {
-      screen_name: user
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-    cache.get(endpoint, user).then(cachedFollowerIds => {
-      if (cachedFollowerIds.length === 0) {
-        client().get(endpoint, params, (error, followerIds, response) => {
-          if (error) {
-            reject(error)
-          } else {
-            cache.put(endpoint, user, followerIds.ids)
-            resolve(followerIds.ids)
-          }
-        })
-      } else {
-        resolve(cachedFollowerIds[0].value)
-      }
-    })
-
-  })
-}
-
-const getFriendIdsForUser = (user) => {
-  const endpoint = 'friends/ids'
-
-  let params = {}
-  if (typeof user === 'number') {
-    params = {
-      user_id: user
-    }
-  } else {
-    params = {
-      screen_name: user
-    }
-  }
-
-  return new Promise((resolve, reject) => {
-    cache.get(endpoint, user).then(cachedFriendsIds => {
-      if (cachedFriendsIds.length === 0) {
-        client().get(endpoint, params, (error, friendIds, response) => {
-          if (error) {
-            reject(error)
-          } else {
-            cache.put(endpoint, user, friendIds.ids)
-            resolve(friendIds.ids)
-          }
-        })
-      } else {
-        resolve(cachedFriendsIds[0].value)
-      }
-    })
-  })
-}
-
-const computeKeyFor = (userIds) => {
-  return userIds.reduce((a, b) => a + b)
-}
-
-const getScreenNamesForUsers = (userIds) => {
-  const cacheKey = computeKeyFor(userIds.split(','))
-  const endpoint = 'users/lookup'
-  const params = {
-    user_id: userIds
-  }
-
-  return new Promise((resolve, reject) => {
-    cache.get(endpoint, cacheKey).then(cachedUsers => {
-      if (cachedUsers.length === 0) {
-        client().get(endpoint, params, (error, users, response) => {
-          if (error) {
-            reject(error)
-          } else {
-            cache.put(endpoint, cacheKey, users)
-            resolve(users)
-          }
-        })
-      } else {
-        resolve(cachedUsers[0].value)
-      }
-    })
-  })
-}
-
-const getScreenNameForAllUsers = (followerIds) => {
-  const promiseArray = []
-  while (followerIds.length > 0) {
-    let ids = []
-    if (followerIds.length > 100) {
-      ids = followerIds.splice(0, 100)
-    } else {
-      ids = followerIds.splice(0)
-    }
-    promiseArray.push(getScreenNamesForUsers(ids.join(',')))
-  }
-  return Promise.all(promiseArray)
-}
+const TwitterClient = require('./twitterClient')
 
 const intersection = (arr1, arr2) => {
   const sortedArr = arr1.concat(arr2).sort()
@@ -125,8 +15,8 @@ const intersection = (arr1, arr2) => {
 
 const getCommonFollowers = (user1, user2) => {
   return Promise.all([
-    getFollowersIdsForUser(user1),
-    getFollowersIdsForUser(user2)
+    TwitterClient.getFollowersIdsForUser(user1),
+    TwitterClient.getFollowersIdsForUser(user2)
   ]).then((followerIds) => {
     return intersection(followerIds[0], followerIds[1])
   })
@@ -134,21 +24,19 @@ const getCommonFollowers = (user1, user2) => {
 
 const getCommonFriends = (user1, user2) => {
   return Promise.all([
-    getFriendIdsForUser(user1),
-    getFriendIdsForUser(user2)
+    TwitterClient.getFriendIdsForUser(user1),
+    TwitterClient.getFriendIdsForUser(user2)
   ]).then((friendIds) => {
     return intersection(friendIds[0], friendIds[1])
   })
 }
 
 const buildGraph = (friendIds) => {
-  console.log("Friends Ids")
-  console.log(friendIds)
   const friendPromises = []
 
   friendIds.forEach(friend => {
     friendPromises.push(
-      getFriendIdsForUser(friend).then(friendsOfFriend => {
+      TwitterClient.getFriendIdsForUser(friend).then(friendsOfFriend => {
         return {id: friend, friends: friendsOfFriend}
       })
     )
@@ -161,14 +49,31 @@ const buildGraph = (friendIds) => {
       graphPromise.push(
         intersection(commonFriends, friend.friends)
         .then( friendsIntersection => {
-          console.log("Intersection")
-          console.log(friendsIntersection)
           return [friend.id, friendsIntersection]
         })
       )
     })
     return Promise.all(graphPromise)
   })
+}
+
+const convertToTGF = (graphContainer) => {
+  const tgfArray = []
+  for(key in graphContainer.userScreenNameMap) {
+    tgfArray.push(`${key} ${graphContainer.userScreenNameMap[key]}\n`)
+  }
+
+  tgfArray.push('#', '\n')
+
+  graphContainer.graph.forEach(user => {
+    const userId = user[0]
+    const friends = user[1]
+    friends.forEach(f => {
+      tgfArray.push(`${userId} ${f}\n`)
+    })
+  })
+
+  return tgfArray.join("")
 }
 
 Promise.all([
@@ -179,6 +84,19 @@ Promise.all([
 }).then((commonFriendsAndFollowersIds) => {
   return buildGraph(commonFriendsAndFollowersIds)
 }).then((friendGraph) => {
-  console.log("The Graph")
-  console.log(friendGraph)
+  const friendsIds = friendGraph.map(f => f[0])
+  return TwitterClient.getScreenNamesForUsers(friendsIds.join(',')).then(users => {
+    // return users.map(user => {return {id: user.id, name: user.screen_name}})
+    return users.reduce((theMap, u) => {
+      theMap[u.id] = u.screen_name;
+      return theMap
+    }, {})
+  }).then(userScreenNameMap => {
+    return {
+      userScreenNameMap: userScreenNameMap,
+      graph: friendGraph
+    }
+  })
+}).then(graphObject => {
+  console.log(convertToTGF(graphObject))
 }).catch(reason => console.log(reason))
--- twitterClient.js
+++ twitterClient.js
@@ -1,5 +1,7 @@
 const Twitter = require('twitter');
 const env = require('node-env-file');
+const CacheManager = require('./cache')
+const cache = new CacheManager('myproject')
 
 env('.env');
 const consumerKeys = process.env.CONSUMER_KEYS.split(',')
@@ -27,4 +29,119 @@ const getTwitter = () => {
   return theClient
 }
 
-module.exports = getTwitter
+const getFollowersIdsForUser = (user) =>  {
+  const endpoint = 'followers/ids'
+
+  let params = {}
+  if (typeof user === 'number') {
+    params = {
+      user_id: user
+    }
+  } else {
+    params = {
+      screen_name: user
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    cache.get(endpoint, user).then(cachedFollowerIds => {
+      if (cachedFollowerIds.length === 0) {
+        getTwitter().get(endpoint, params, (error, followerIds, response) => {
+          if (error) {
+            reject(error)
+          } else {
+            cache.put(endpoint, user, followerIds.ids)
+            resolve(followerIds.ids)
+          }
+        })
+      } else {
+        resolve(cachedFollowerIds[0].value)
+      }
+    })
+
+  })
+}
+
+const getFriendIdsForUser = (user) => {
+  const endpoint = 'friends/ids'
+
+  let params = {}
+  if (typeof user === 'number') {
+    params = {
+      user_id: user
+    }
+  } else {
+    params = {
+      screen_name: user
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    cache.get(endpoint, user).then(cachedFriendsIds => {
+      if (cachedFriendsIds.length === 0) {
+        getTwitter().get(endpoint, params, (error, friendIds, response) => {
+          if (error) {
+            reject(error)
+          } else {
+            cache.put(endpoint, user, friendIds.ids)
+            resolve(friendIds.ids)
+          }
+        })
+      } else {
+        resolve(cachedFriendsIds[0].value)
+      }
+    })
+  })
+}
+
+const computeKeyFor = (userIds) => {
+  return userIds.reduce((a, b) => a + b)
+}
+
+const getScreenNamesForUsers = (userIds) => {
+  const cacheKey = computeKeyFor(userIds.split(','))
+  const endpoint = 'users/lookup'
+  const params = {
+    user_id: userIds
+  }
+
+  return new Promise((resolve, reject) => {
+    cache.get(endpoint, cacheKey).then(cachedUsers => {
+      if (cachedUsers.length === 0) {
+        getTwitter().get(endpoint, params, (error, users, response) => {
+          if (error) {
+            reject(error)
+          } else {
+            cache.put(endpoint, cacheKey, users)
+            resolve(users)
+          }
+        })
+      } else {
+        resolve(cachedUsers[0].value)
+      }
+    })
+  })
+}
+
+const getScreenNameForAllUsers = (followerIds) => {
+  const promiseArray = []
+  while (followerIds.length > 0) {
+    let ids = []
+    if (followerIds.length > 100) {
+      ids = followerIds.splice(0, 100)
+    } else {
+      ids = followerIds.splice(0)
+    }
+    promiseArray.push(getScreenNamesForUsers(ids.join(',')))
+  }
+  return Promise.all(promiseArray)
+}
+
+
+
+module.exports = {
+  getScreenNameForAllUsers: getScreenNameForAllUsers,
+  getScreenNamesForUsers: getScreenNamesForUsers,
+  getFriendIdsForUser: getFriendIdsForUser,
+  getFollowersIdsForUser: getFollowersIdsForUser
+}
